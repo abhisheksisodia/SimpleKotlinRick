54733475eb5cc2ec9a473769ce17da164a32c37a
Abhishek Sisodia <abhisheksisodia13@gmail.com>
2024-02-12 15:57:01 -0500

    added networking module and ktor

--- RickMorty/app/src/main/java/com/abhishek/rickmorty/MainActivity.kt
+++ RickMorty/app/src/main/java/com/abhishek/rickmorty/MainActivity.kt
@@ -3,23 +3,50 @@ package com.abhishek.rickmorty
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.abhishek.networking.KtorClient
+import com.abhishek.networking.Character
 import com.abhishek.rickmorty.ui.theme.RickMortyTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
+    private val ktorClient = KtorClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var character by remember {
+                mutableStateOf<Character?>(null)
+            }
+
+            LaunchedEffect(key1 = Unit, block = {
+                delay(3000)
+                character = ktorClient.getCharacter(55)
+            })
+
             RickMortyTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column {
+                        Greeting("Android")
+
+                        Text(text = character?.name ?: "No character")
+                    }
                 }
             }
         }
@@ -29,8 +56,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-            text = "Hello $name!",
-            modifier = modifier
+        text = "Hello $name!",
+        modifier = modifier
     )
 }
 
