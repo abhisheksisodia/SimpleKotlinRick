04b5295a8f443791f43ea1418bdc0f4d4c08aec2
Abhishek Sisodia <abhisheksisodia13@gmail.com>
2024-02-12 23:53:19 -0500

    added coil and details screen

--- RickMorty/app/src/main/java/com/abhishek/rickmorty/MainActivity.kt
+++ RickMorty/app/src/main/java/com/abhishek/rickmorty/MainActivity.kt
@@ -3,67 +3,33 @@ package com.abhishek.rickmorty
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.abhishek.networking.KtorClient
-import com.abhishek.networking.models.domain.Character
+import com.abhishek.rickmorty.screens.CharacterDetailsScreen
+import com.abhishek.rickmorty.ui.theme.RickPrimary
 import com.abhishek.rickmorty.ui.theme.RickMortyTheme
-import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
+
     private val ktorClient = KtorClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var character by remember {
-                mutableStateOf<Character?>(null)
-            }
-
-            LaunchedEffect(key1 = Unit, block = {
-                delay(3000)
-                character = ktorClient.getCharacter(25)
-            })
-
             RickMortyTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = RickPrimary
                 ) {
-                    Column {
-                        Greeting("Android")
-                        Text(text = character?.name ?: "No character")
-                    }
+                    CharacterDetailsScreen(
+                        ktorClient = ktorClient,
+                        characterId = 1
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RickMortyTheme {
-        Greeting("Android")
     }
 }
---
+++ RickMorty/app/src/main/java/com/abhishek/rickmorty/components/character/CharacterDetailsNamePlateComponent.kt
@@ -0,0 +1,45 @@
+package com.abhishek.rickmorty.components.character
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.abhishek.networking.models.domain.CharacterStatus
+import com.abhishek.rickmorty.components.CharacterStatusComponent
+import com.abhishek.rickmorty.ui.theme.RickAction
+
+@Composable
+fun CharacterDetailsNamePlateComponent(name: String, status: CharacterStatus) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        CharacterStatusComponent(characterStatus = status)
+        Text(
+            text = name,
+            fontSize = 42.sp,
+            lineHeight = 42.sp,
+            fontWeight = FontWeight.Bold,
+            color = RickAction
+        )
+    }
+}
+
+@Preview
+@Composable
+fun NamePlatePreviewAlive() {
+    CharacterDetailsNamePlateComponent(name = "Rick Sanchez", status = CharacterStatus.Alive)
+}
+
+@Preview
+@Composable
+fun NamePlatePreviewDead() {
+    CharacterDetailsNamePlateComponent(name = "Rick Sanchez", status = CharacterStatus.Dead)
+}
+
+@Preview
+@Composable
+fun NamePlatePreviewUnknown() {
+    CharacterDetailsNamePlateComponent(name = "Rick Sanchez", status = CharacterStatus.Unknown)
+}
